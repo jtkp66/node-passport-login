fa1253ca035e318c6ae0ff025e4766f523fe09b7
Jamie Pham <jtkp66@gmail.com>
2019-01-08 09:18:35 -0800

    added mongoose, mongoose schema, and error partial for validation

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,38 @@ router.get('/login', (req, res) => res.render('login'));
 // Register page
 router.get('/register', (req, res) => res.render('register'));
 
+// Register Handle
+router.post('./register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    //check required fields
+    if(!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill out all fields'});
+    }
+
+    // Check passwords match
+    if(password !== password2) {
+        errors.push({ msg: 'Passwords do not match'});
+    }
+
+    // Check pw length
+    if(password.length < 6) {
+        errors.push({ msg: 'Password should be at least 6 characters' });
+    }
+
+    if(errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    } else {
+        res.send('pass');
+    }
+
+});
+
 module.exports = router;
